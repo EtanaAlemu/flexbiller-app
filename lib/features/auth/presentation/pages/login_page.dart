e6fdc0c0b921b45d9df2946d7fc745331a90d539
EtanaAlemu <etanaalemunew@gmail.com>
2025-08-29 12:20:24 +0300

    refactor: clean up login page by removing debug elements and demo buttons

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../../core/theme/theme_toggle_widget.dart';
 import '../../../../core/localization/language_selector_widget.dart';
-import '../../../../core/widgets/jwt_info_widget.dart';
 import '../widgets/login_form.dart';
-import '../../../subscriptions/presentation/pages/subscriptions_demo_page.dart';
-import '../../../subscriptions/presentation/pages/subscription_details_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -24,40 +21,7 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              LoginForm(),
-              SizedBox(height: 32),
-              JwtInfoWidget(showDetails: true),
-              SizedBox(height: 16),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SubscriptionsDemoPage(),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.subscriptions),
-                label: const Text('View Recent Subscriptions'),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton.icon(
-                onPressed: () {
-                  // Test with a sample subscription ID
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SubscriptionDetailsPage(
-                        subscriptionId: '8a0075a7-104c-4dfc-9e9a-6737c51cd59c',
-                      ),
-                    ),
-                  );
-                },
-                icon: const Icon(Icons.info),
-                label: const Text('Test Subscription Details'),
-              ),
-            ],
-          ),
+          child: Column(children: [LoginForm()]),
         ),
       ),
     );
