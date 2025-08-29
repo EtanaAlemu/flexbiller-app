2648d7bb2256ade3f3647891d0173a21340fa62a
EtanaAlemu <etanaalemunew@gmail.com>
2025-08-29 12:19:59 +0300

    refactor: update app constants to use build configuration and improve timeouts

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,21 +1,23 @@
+import '../config/build_config.dart';
+
 class AppConstants {
   // API Configuration
-  static const String baseUrl = 'https://flexbillerapi.aumtech.org/api';
-  static const int connectionTimeout = 5000;
-  static const int receiveTimeout = 3000;
-  
+  static String get baseUrl => '${BuildConfig.baseUrl}/api';
+  static int get connectionTimeout =>
+      BuildConfig.connectionTimeout.inMilliseconds;
+  static int get receiveTimeout => 10000; // 10 seconds for receive timeout
+
   // Storage Keys
   static const String authTokenKey = 'auth_token';
   static const String refreshTokenKey = 'refresh_token';
   static const String userIdKey = 'user_id';
   static const String userEmailKey = 'user_email';
-  
+
   // Database
   static const String databaseName = 'flexbiller.db';
   static const int databaseVersion = 1;
-  
+
   // App Info
   static const String appName = 'FlexBiller';
   static const String appVersion = '1.0.0';
 }
-
